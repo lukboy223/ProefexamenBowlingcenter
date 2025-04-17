5dd78aa586bae5af47cef77619a4aadcfa537604
lukboy223 <ltvanekeren@gmail.com>
2025-04-17 11:36:25 +0200

    update done

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -138,6 +138,74 @@ class ReservationController extends Controller
     }
 
     public function update(Request $request, $id){
+
+        $request->validate([
+            'CustomerId' => 'required|integer',
+            'BowlingLaneId' => 'required|integer|exists:BowlingLanes,id',
+            'ReservationDate' => 'required|date',
+            'ReservationTime' => 'required|date_format:H:i:s',
+            'AmountOfHours' => 'required|integer|min:1|max:10',
+            'Adults' => 'required|integer|min:1|max:8',
+            'Kids' => 'required|integer|min:0|max:4',
+            'Extra' => 'required|integer| exists:extras,id',
+        ]);
+
+        try{
+
+            $reservationDate = $request->input('ReservationDate');
+            $reservationTime = $request->input('ReservationTime');
+            $amountOfHours = $request->input('AmountOfHours');
+            
+            $dayOfWeek = date('N', strtotime($reservationDate)); // 1 (Monday) to 7 (Sunday)
+            $time = strtotime($reservationTime);
+            
+            if ($dayOfWeek >= 1 && $dayOfWeek <= 4) {
+                // Monday to Thursday
+                $pricePerHour = 24.00;
+            } elseif ($dayOfWeek >= 5 && $dayOfWeek <= 7) {
+                // Friday to Sunday
+                if ($time >= strtotime('14:00') && $time < strtotime('18:00')) {
+                    $pricePerHour = 28.00;
+                } elseif ($time >= strtotime('18:00') && $time <= strtotime('24:00')) {
+                    $pricePerHour = 33.50;
+                } else {
+                    $pricePerHour = 24.00; // Default to weekday price if outside specified times
+                }
+            } else {
+                $pricePerHour = 24.00; // Default price
+            }
+            if($request->BowlingLaneId == 2){
+                $BowlingLaneId = 7;
+            }else{
+                $BowlingLaneId = 2;
+            }
+            
+            // Calculate the total price
+            $price = $pricePerHour * $amountOfHours;
+
+
+            DB::select('call sp_update_reservation(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+                $id,
+                $request->input('CustomerId'),
+                $BowlingLaneId,
+                $request->input('ReservationDate'),
+                $request->input('ReservationTime'),
+                $request->input('AmountOfHours'),
+                $price,
+                $request->input('Adults'),
+                $request->input('Kids'),
+                $request->input('Extra')
+            ]);
+
+            return redirect()->route('reservation.index')->with('success', 'Reservering succesvol aangepast.');
+        
+
+        }catch (\Exception $e) {
+            //logs the error in the log
+            Log::error('error updating reservation: ' . $e->getMessage());
+            //redirects the user to the index page with an error message
+            return redirect()->route('reservation.edit', $id)->with('error', 'Er is iets fout gegaan bij het wijzigen van de reservering');
+        }
         dd($request);
     }
 
--- database/migrations/2025_04_10_140202_sp_update_reservation.php
+++ database/migrations/2025_04_10_140202_sp_update_reservation.php
@@ -15,12 +15,48 @@ return new class extends Migration
         DB::unprepared('
         drop procedure if exists sp_update_reservation;
         create procedure sp_update_reservation(
-         in givReservationId int
+
+        in givReservationId int unsigned,
+        in INContactIdId int unsigned,
+        in INBowlingLaneId int unsigned,
+
+        in INReservationDate date,
+        in INReservationTime time,
+        in INAmountOfHours tinyint unsigned,
+        in INPrice decimal(5,2),
         
+        in INAdults tinyint unsigned,
+        in INKids tinyint unsigned,
+        
+        in INExtraId int unsigned
         )
         begin
 
+         declare SELCustomerId int unsigned default 0;
+        
+        select 
+        CUST.Id
+        into SELCustomerId
+        from Contacts as CONT
+        
+        inner join Users as USER
+        on CONT.Id = USER.ContactId
+        
+        inner join Customers as CUST
+        on CUST.UserId = USER.Id
 
+        where CONT.Id = INContactIdId;
+
+
+        update reservations set
+        CustomerId = SELCustomerId,
+    BowlingLaneId = INBowlingLaneId,
+    ExtrasId = INExtraId,
+    ReservationDate =INReservationDate,
+    ReservationTime = INReservationTime,
+    AmountOfHours = INAmountOfHours,
+    Price = INPrice
+    where Id = givReservationId;
 
         end
         ');
